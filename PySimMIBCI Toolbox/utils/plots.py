339f5bed68dcd8cc0938bb6d839e0542c44c9ae8
Catalina M. Galván <79813952+catalinamagalvan@users.noreply.github.com>
2023-01-24 12:02:42 -0300

    Add files via upload

--- PySimMIBCI Toolbox/utils/plots.py
+++ PySimMIBCI Toolbox/utils/plots.py
@@ -1,8 +1,5 @@
-# -*- coding: utf-8 -*-
 """
-Created on Wed Jun  9 10:55:19 2021
-
-@author: catal
+# Author: Catalina M. Galvan <cgalvan@santafe-conicet.gov.ar>
 """
 
 import numpy as np
@@ -11,7 +8,29 @@ import matplotlib.pyplot as plt
 
 def plot_raw_2_channels(raw, save_name=None, save=False, start=30,
                         duration=20):
-    """Plot a set of channels in raw data."""
+    """
+    Plot C3 and C4 channels of an EEG segment.
+
+    Parameters
+    ----------
+    raw : Instance of MNE Raw
+        The corresponding MNE Raw.
+    save_name : str, optional
+        The name of the file where the plot will be saved if save=True. The
+        default is None.
+    save : bool, optional
+        Whether to save the plot. The default is False.
+    start : float, optional
+        Time in seconds of the start of the segment to plot. The default is 30.
+    duration : float, optional
+        Time in seconds of the duration of the segment to plot. The default is
+        20.
+
+    Returns
+    -------
+    None.
+
+    """
     indices = [raw.ch_names.index('C3'), raw.ch_names.index('C4')]  # C3 y C4
     data_eeg = raw.get_data(picks='eeg')
 
@@ -23,7 +42,6 @@ def plot_raw_2_channels(raw, save_name=None, save=False, start=30,
 
     fig, axs = plt.subplots(1, 2, figsize=(8, 3), constrained_layout=True,
                             sharex=True, sharey=True)
-    # fig.suptitle(title, fontsize=16)
     for i, ax in enumerate(axs.flat):
         line, = ax.plot(temporal_line, data_eeg[indices[i],
                                                 int(start*raw.info['sfreq']):
